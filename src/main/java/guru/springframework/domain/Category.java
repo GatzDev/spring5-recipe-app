8e65ec9a984b51f3d04d84fd819e612386f6f117
V.Valchanov <v.valchanov@gmail.com>
2023-10-17 16:18:15 +0300

    Added repositories querys.

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -17,23 +17,18 @@ public class Category {
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
     public Set<Recipe> getRecipes() {
         return recipes;
     }
-
     public void setRecipes(Set<Recipe> recipes) {
         this.recipes = recipes;
     }
